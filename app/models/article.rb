c5ac6eba6c29683643a6ec25e2c905f1f0fdf17e
eiffel qiu <eiffelqiu@gmail.com>
2008-12-02 21:35:11 +0800

    auto update slug field according to the rule by replace title's blank to '_'

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,5 +8,12 @@ class Article
   property :body, Text, :lazy => false # work around solution, fixed in the future
   property :title, String
   property :updated_at, DateTime
+  
+  before :valid?, :set_permalink
 
+  # our callback needs to accept the context used in the validation,
+  # even if it ignores it, as #save calls #valid? with a context.
+  def set_permalink(context = :default)
+    self.slug = title.gsub(/\s+/,'_')
+  end
 end
