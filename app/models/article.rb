26663e93af48a05839df1f4180491d81fa9f42ac
eiffel qiu <eiffelqiu@gmail.com>
2008-12-02 21:22:37 +0800

    work around solution for dm 0.9.7 problem.

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,7 +5,7 @@ class Article
 
   property :slug, String
   property :created_at, DateTime
-  property :body, Text, :lazy => false
+  property :body, Text, :lazy => false # work around solution, fixed in the future
   property :title, String
   property :updated_at, DateTime
 
